7fe4aef3c937d0be819af94d1248f4fbdc40b26d
Pavel Zhuravlev <hairol97@gmail.com>
2021-08-20 13:04:58 +0400

    Create play_yt.sh

---
+++ play_yt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Using script with shortcut & clipboard
+#url="$(xclip -o -selection clipboard)"
+url="$1"
+
+#Max resolution
+RES="1080"
+
+[ -z "$url" ] && exit 1
+YOU_DL="$(which youtube-dl)"
+full_data="$($YOU_DL -e -g -f "bestvideo[height<=?$RES][ext=mp4]"+worstaudio/"best[height<=?$RES]" "$url")"
+TITLE="$(sed -n 1p <<< $full_data)"
+URL_VIDEO="$(sed -n 2p <<< $full_data)"
+URL_AUDIO="$(sed -n 3p <<< $full_data)"
+
+vlc -q -d "$URL_VIDEO" --input-slave "$URL_AUDIO" --meta-title "$TITLE"
