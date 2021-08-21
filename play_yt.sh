83f7eeb52d3cf15aa723d0afdfacd9d731e2d9d7
Pavel Zhuravlev <hairol97@gmail.com>
2021-08-22 00:28:50 +0400

    Update play_yt.sh

--- play_yt.sh
+++ play_yt.sh
@@ -10,7 +10,11 @@ RES="1080"
 
 [ -z "$url" ] && exit 1
 YOU_DL="$(which youtube-dl)"
-full_data="$($YOU_DL -e -g -f "bestvideo[height<=?$RES][ext=mp4]"+worstaudio/"best[height<=?$RES]" "$url")"
+
+full_data="$($YOU_DL -e -g -f "bestvideo[height<=?$RES][ext=mp4][vcodec^=avc1]"+worstaudio/"best[height<=?$RES]" "$url")"
+# If you use HDR 
+#full_data="$($YOU_DL -e -g -f "bestvideo[height<=?$RES][ext=mp4]"+worstaudio/"best[height<=?$RES]" "$url")"
+
 TITLE="$(sed -n 1p <<< $full_data)"
 URL_VIDEO="$(sed -n 2p <<< $full_data)"
 URL_AUDIO="$(sed -n 3p <<< $full_data)"
